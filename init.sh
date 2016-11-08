6d7a71def122dd965aa5ed4e0bee23e818494c92
kna986 <kna986@gmail.com>
2016-11-08 14:02:14 +0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
-ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo rm -rf /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx start
-cd /home/box/web/ask/
-sudo gunicorn -c /home/box/web/etc/gunicorn.conf ask.wsgi -D
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application
+sudo /etc/init.d/gunicorn restart
